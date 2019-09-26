2f219893e7d91ef0177b3170826fcab53ec1e7db
Jamson <1940318814@qq.com>
2019-09-26 15:47:59 +0800

    finished with .mat file

--- pyfile/dnn_model.py
+++ pyfile/dnn_model.py
@@ -1,11 +1,12 @@
 import os
+import math
 import numpy as np
 import tensorflow as tf
 import pandas as pd
 
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
-from sklearn import train_test_split
+from sklearn.model_selection import train_test_split
 from tensorflow.python import ops
 
 
@@ -43,7 +44,7 @@ def forward_propagation(X, parameters):
     A = X
 
     for i in range(num_layers):
-        Z = tf.add(tf.matmul(A, parameters['W'+str(i+1)]), parameters['b'+str(i+1)])
+        Z = tf.add(tf.matmul(parameters['W' + str(i + 1)], A), parameters['b' + str(i + 1)])
         A = tf.nn.relu(Z)
 
     return Z
@@ -78,7 +79,7 @@ def compute_cost(Z, Y, beta, l2_loss):
     logits = tf.compat.v1.transpose(Z)
     labels = tf.compat.v1.transpose(Y)
 
-    cost = tf.compat.v1.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=logits)) + beta*l2_loss
+    cost = tf.compat.v1.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels)) + beta*l2_loss
 
     return cost
 
@@ -99,7 +100,7 @@ def mini_batch(X, Y, batch_size):
     shuffled_x = X[:, permutation]
     shuffled_y = Y[:, permutation]
 
-    num_batches = np.floor(m / batch_size)
+    num_batches = math.floor(m / batch_size)
     for k in range(0, num_batches):
         batch_x = shuffled_x[:, k*batch_size: k*batch_size+batch_size]
         batch_y = shuffled_y[:, k*batch_size: k*batch_size+batch_size]
@@ -113,7 +114,7 @@ def mini_batch(X, Y, batch_size):
     return batches
 
 
-def model(layer_dims, train_X, train_Y, test_X, test_Y, beta=0,learning_rate=0.0001, epochs=10000, batch_size=32, print_cost=True, plot_cost=True):
+def model(layer_dims, train_X, train_Y, test_X, test_Y, beta=0.0, learning_rate=0.0001, epochs=10000, batch_size=32, print_cost=True, plot_cost=True):
 
     ops.reset_default_graph()
 
@@ -127,8 +128,8 @@ def model(layer_dims, train_X, train_Y, test_X, test_Y, beta=0,learning_rate=0.0
     parameters = init_parameters(layer_dims)
     l2_loss = compute_l2_loss(parameters)
 
-    Y_hat = forward_propagation(train_X, parameters)
-    cost = compute_cost(Z=Y_hat, beta=beta, Y=train_Y, l2_loss=l2_loss)
+    Y_hat = forward_propagation(X, parameters)
+    cost = compute_cost(Z=Y_hat, beta=beta, Y=Y, l2_loss=l2_loss)
 
     optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
     train_step = optimizer.minimize(cost)
@@ -155,21 +156,20 @@ def model(layer_dims, train_X, train_Y, test_X, test_Y, beta=0,learning_rate=0.0
             if plot_cost and epoch % 5 == 0:
                 costs.append(epoch_cost)
 
-            pred = tf.equal(tf.argmax(Y_hat), tf.argmax(train_Y))
-            accuracy = tf.reduce_mean(tf.cast(pred), float)
-            print('Train accuracy after epoch %i: %.2f' % (epoch, accuracy.eval({X: train_X, Y: train_Y})))
-            print('Test accuracy after epoch %i: %.2f' % (epoch, accuracy.eval({X: test_X, Y: test_Y})))
+            pred = tf.equal(tf.argmax(Y_hat), tf.argmax(Y))
+            accuracy = tf.reduce_mean(tf.cast(pred, tf.float32))
+            print('Train accuracy after epoch %i: %.2f Test accuracy %.2f' % (epoch, accuracy.eval({X: train_X, Y: train_Y}), accuracy.eval({X: test_X, Y: test_Y})))
 
         if plot_cost:
             plt.plot(np.squeeze(costs))
             plt.ylabel('cost')
             plt.xlabel('iterations (per fives)')
-            plt.title('Learning rate='+str(learning_rate))
+            plt.title('Train with learning rate decay')
             plt.show()
 
         parameters = sess.run(parameters)
 
-    return  parameters
+    return parameters
 
 
 def get_set_data(dir_path):
@@ -201,7 +201,7 @@ def get_set_data(dir_path):
 def get_train_test_from_csv(dir_path):
     """
     read data form of csv
-    :param dir_path: the dataset path
+    :param dir_path: the data set path
     :return:train_x, train_y, test_x, test_y
     """
     train_path = os.path.join(dir_path, 'train_set')
@@ -210,13 +210,30 @@ def get_train_test_from_csv(dir_path):
     train_data = get_set_data(train_path)
     test_data = get_set_data(test_path)
 
-    train_x = train_data[, : -2]
+    train_x = train_data[:, :-2]
     train_y = train_data[:, -1]
 
-    test_x = test_data[, : -2]
+    test_x = test_data[:, :-2]
     test_y = test_data[:, -1]
 
-    return train_x.T, train_y, test_x.T, test_y
+    train_x = train_x.reshape(train_x.shape[0], -1).T
+    train_y = train_y.reshape(train_y.shape[0], -1).T
+
+    test_x = test_x.reshape(test_x.shape[0], -1).T
+    test_y = test_y.reshape(test_y.shape[0], -1).T
+
+    return train_x, train_y, test_x, test_y
+
+
+def convert_to_one_hot(y, classes):
+    """
+    convert a vector to one hot encode
+    :param y: true label
+    :param classes:number of class
+    :return: one hot matrix
+    """
+    y = np.eye(classes)[y.reshape(-1)].T
+    return y
 
 
 def get_train_test_from_mat(dir_path, feature_name, label_name, test_size):
@@ -235,17 +252,33 @@ def get_train_test_from_mat(dir_path, feature_name, label_name, test_size):
     label_path = os.path.join(dir_path, label_name)
     label_path = label_path + '.mat'
     label_data = loadmat(label_path).get(label_name)
+    label_data -= 1
 
     train_x, test_x, train_y, test_y = train_test_split(train_data, label_data, test_size=test_size)
+
+    train_x = train_x.reshape(train_x.shape[0], -1).T
+    train_y = train_y.reshape(train_y.shape[0], -1).T
+
+    test_x = test_x.reshape(test_x.shape[0], -1).T
+    test_y = test_y.reshape(test_y.shape[0], -1).T
 
     return train_x, train_y, test_x, test_y
 
 
 if __name__ == '__main__':
 
-    # data = loadmat('./datasets/feature.mat').get('feature')
-    # label = loadmat('./datasets/label.mat').get('label')
+    classes = 3  # number of class
+    dirPath = '../datasets/'
+    featureName = 'feature'
+    labelName = 'label'
 
-    classes = 3
+    train_x, train_y, test_x, test_y = get_train_test_from_mat(dir_path=dirPath, feature_name=featureName, label_name=labelName, test_size=0.2)
 
-    layer_dims = [data.shape[0], 10, 6, 3, classes]
+    train_y = convert_to_one_hot(train_y, classes)
+    test_y = convert_to_one_hot(test_y, classes)
+
+    layer_dims = [train_x.shape[0], 10, 4, classes]
+
+    parameter = model(layer_dims=layer_dims, train_X=train_x, train_Y=train_y, test_X=test_x, test_Y=test_y, beta=0.01, learning_rate=0.001, epochs=1000)
+
+    tf.saved_model.save(parameter, './model')
